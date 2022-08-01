95152aa666310b67883e17e70e4e3931ea0152b7
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2022-08-01 03:34:42 +0000

    Done API get newest comic

--- index.js
+++ index.js
@@ -3,39 +3,38 @@ const app = express()
 const port = 3000
 const axios = require("axios");
 const cheerio = require("cheerio");
-
+const PAGE_URL = "http://www.nettruyenme.com/hot"
 
 app.get('/', (req, res) => {
-    res.json('Hello World!')
+    res.send('Hello World!')
 })
 
-app.get('/hot', (req, res) => {
-    res.json(getData())
+app.get('/hot', async (req, res) => {
+    var data = await scraper();
+    res.send(data)
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App listening on port ${port}`)
 })
 
- function getData() {
-    const urlNettruyen = "http://www.nettruyenme.com/"
+const scraper = async () => {
+    const response = await axios.get(PAGE_URL);
+    const $ = cheerio.load(response.data);
+
     const data = []
-     axios.get(urlNettruyen).then((response) => {
-        const htmlRaw = response.data
-        const $ = cheerio.load(htmlRaw)
+    const selectList = "figure > div > a > img"
 
-        const selectList = "figure > div > a > img"
-
-        $(selectList).each((index, element) => {
-            const img = "http:" + $(element).attr("data-original")
-            const title = $(element).attr("alt")
-            data.push({
-                title,
-                img
-            })
+    $(selectList).each((index, element) => {
+        const link = $(element).parent().attr("href")
+        const img = "http:" + $(element).attr("data-original")
+        const title = $(element).attr("alt")
+        data.push({
+            title,
+            thumbnail: img,
+            link
         })
-
-        console.log(data)
-        return data
     })
-}
+
+    return data
+};
